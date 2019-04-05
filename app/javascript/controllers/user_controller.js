cf0e1b945d576aeee6959fa9f650ec6935dabbfc
Tomáš Celizna <tomas.celizna@gmail.com>
2019-04-05 15:47:25 +0200

    dirty!

--- app/javascript/controllers/user_controller.js
+++ app/javascript/controllers/user_controller.js
@@ -4,10 +4,22 @@ import _ from 'lodash'
 
 export default class extends Controller {
   initialize() {
+    this.dirtyFields = [];
     this.onKeyUp = _.debounce(this.onKeyUp, 500)
+
+    document.addEventListener('cable-ready:after-morph', (e) => {
+      for (let id of this.dirtyFields) {
+        let el = document.querySelector("#"+id);
+        el.parentNode.classList.add('dirty');
+      }
+    })
   }
 
-  onKeyUp() {
+  onKeyUp(e) {
+    if (this.dirtyFields.includes(e.target.id) == false) {
+      this.dirtyFields.push(e.target.id);
+    }
+
     let email = document.querySelector('#user_email').value;
     let age = document.querySelector('#user_age').value;
 
